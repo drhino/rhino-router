ec37ab18f295f471e6ba3d61dc6ea57ab3806bd1
Gerd <gerdachternaam@gmail.com>
2021-05-13 20:46:45 +0200

    v3

--- index.js
+++ index.js
@@ -1,141 +1,150 @@
 /**
- * Router.
+ * rhino-router
  */
-export default class
+
+// Holds the defined routes.
+let routes = []
+
+/**
+ * Adds a new route pattern.
+ *
+ * The `route` key is required.
+ * The following keys are reserved: `pattern`, `placeholders`, `vars`.
+ *
+ * @param {object} input { route: '/', ...(custom) }
+ *
+ * @throws {Error} RR01xx: Required key missing.
+ * @throws {Error} RR02xx: Disallowed key used in input.
+ */
+export const add = (input) =>
 {
-    // Immutable.
-    #routes = []
+    if (typeof input.route === 'undefined')
+        throw new Error('RR0100: Please add a route key to your input object.')
 
-    /**
-     * Parses the input routes.
-     * @param {array} routes
-     */
-    constructor (routes)
-    {
-        for (const item of routes) {
+    if (input.pattern)
+        throw new Error('RR0200: The key: "pattern" is already used by the router class.')
 
-            let route = item.route || item,
-                attr  = item.attr,
-                keys  = []
+    if (input.placeholders)
+        throw new Error('RR0201: The key: "placeholders" is already used by the router class.')
 
-            // Wether the route query is dynamic.
-            if (route.includes('{')) {
-                const parsed = this.routeParser(route)
+    if (input.vars)
+        throw new Error('RR0202: The key: "vars" is already used by the router class.')
 
-                route = parsed.route
-                keys  = parsed.keys
-            }
+    routes.push( { ...input, ..._parse(input.route) } )
+}
 
-            this.#routes.push( { route, keys, attr } )
+/**
+ * Match route.
+ *
+ * @param {string} pathname Input to match with route pattern.
+ *
+ * @returns {object}  item  { route, pattern, placeholders, vars, ...(custom) }
+ * @returns {boolean} FALSE  No matching route found.
+ */
+export const dispatch = (pathname) =>
+{
+    // Processes each route until a match is found.
+    for (const item of routes) {
+
+        // Returns FALSE || Object
+        const vars = _match(item.pattern, item.placeholders, pathname)
+
+        // Either an empty object (in the case of a fixed route pattern)
+        // OR an object with the placeholders and their values was returned.
+        if (false !== vars) {
+            return { ...item, vars }
         }
     }
 
-    /**
-     * Generates a regex and seperates the route query keys.
-     * @see https://github.com/nikic/FastRoute/blob/master/src/RouteParser/Std.php
-     * @param {string} route
-     * @returns { (string)route, (array)keys }
-     */
-    routeParser (route) {
-        /*
-            \{\s*([a-zA-Z_][a-zA-Z0-9_-]*)\s*(?::\s*([^{}]*
-                (?:\{(?-1)\}[^{}]*)*
-            ))?\}
+    // Searched all routes — No match was found.
+    return false
+}
 
-            \{\s*([a-zA-Z_][a-zA-Z0-9_-]*)\s*(?::\s*([^{}]*))?\}
+/**
+ * Generates a regex and seperates the route placeholders.
+ * ...except when the character: "{" is NOT found in the pattern, then
+ * the value is used as-is. In that case, regex will NOT be used in _match().
+ *
+ * @param {string} pattern
+ *
+ * @returns { pattern, placeholders }
+ *
+ * {string} pattern      The same value in case of a fixed pattern.
+ *                       OR the pattern with capture groups, without placeholders.
+ * {array}  placeholders The placeholder keys or an empty array.
+ */
+const DEFAULT_RGX = '[^/]+'
 
-            /\{([a-zA-Z0-9]*)(?::([^{}]*))?\}/g
-            /\{([a-zA-Z_][a-zA-Z0-9_-]*)(?::([^}]*))?\}/g
-        */
-        const regex = /\{([^:}]*)(?::([^}]*))?\}/g
-        const found = route.matchAll(regex)
+const _parse = (pattern) =>
+{
+    // Found placeholders in dynamic route.
+    let placeholders = []
 
-        let keys = []
+    // Only build the regex when the route has a dynamic pattern.
+    if (pattern.includes('{')) {
 
-        for (const match of found) {
-            // e.g: .* || [0-9]+ || \d+
-            const rgx = match[2]
-                ? '(' + match[2] + ')' // {varName2:\d+} -> (\d+)
-                : '([^/]+)'            // {varName}      -> ([^/]+)
+        const matchAll = pattern.matchAll(/\{([^:}]*)(?::([^}]*))?\}/g)
+        // ...skips whitespace, tabs & line breaks:
+        // /\{\s*([^:}]*)\s*(?::\s*([^}]*))?\}/g
 
-            // Build matching regex.
-            route = route.replace(match[0], rgx)
-            // Saves the key name.
-            keys.push(match[1])
+        for (const match of matchAll) {
+            // Uses the regex pattern in a capturing group.
+            // {varName:\d+}    -> (\d+)
+            // {varName:[0-9]+} -> ([0-9]+)
+            // {varName:.*}     -> (.*)
+            const rgx = '(' + (match[2] || DEFAULT_RGX) + ')'
+
+            // Build regex.
+            pattern = pattern.replace(match[0], rgx)
+            
+            // Saves the placeholder name.
+            placeholders.push(match[1])
         }
 
-        // The route should start and end with the query.
-        route = '^' + route + '$'
-
-        return { route, keys }
+        // The pathname should start and end with the pattern.
+        pattern = '^' + pattern + '$'
     }
 
-    /**
-     * Returns the parsed routes. Useful for debugging.
-     */
-    get routes() {
-        return this.#routes
-    }
+    return { pattern, placeholders }
+}
 
-    /**
-     * Match route.
-     *
-     * @param {string} pathname HTTP request URI.
-     *
-     * @returns { vars, attr } Matching route variables & custom attributes.
-     * @returns {boolean} FALSE No matching route found.
-     */
-    match (pathname) {
-        // Processes each route until a match is found.
-        for (const item of this.#routes) {
-            // Match static or dynamic route.
-            const vars = this.find(item, pathname)
+/**
+ * Checks wether the pathname matches the route pattern.
+ *
+ * @param {string} pattern      Regex or fixed pattern.
+ * @param {array}  placeholders Found placeholders in dynamic route.
+ * @param {string} pathname     Input to match with route pattern.
+ *
+ * @returns {boolean} FALSE Pathname does not match pattern.
+ * @returns {object}  vars  Placeholders with matched values.
+ * @returns {object}  vars  Empty object when a fixed pattern matches.
+ */
+const _match = (pattern, placeholders, pathname) =>
+{
+    let matched, vars = {}
 
-            // Returns an empty object or the found route variables.
-            if (false !== vars) {
-                return { vars, attr: item.attr }
-            }
+    // Calculate the length once for the 'for' loop.
+    const len = placeholders.length
+
+    // Regex match.
+    if (len) {
+        matched = pathname.match(pattern)
+
+        if (null === matched) {
+            return false
         }
-
-        // No match found.
+    }
+    // Fixed pattern.
+    else if (pathname !== pattern) {
         return false
     }
 
-    /**
-     * Checks wether the route matches.
-     *
-     * @param {object} item     { route, keys, attr }
-     * @param {string} pathname HTTP request URI.
-     *
-     * @returns {boolean} FALSE Route does not match.
-     * @returns {object}  vars  Empty or parsed variables.
-     */
-    find (item, pathname)
-    {
-        let matched, vars = {}
-
-        const { route, keys } = item
-
-        // Regex match.
-        if (keys.length) {
-            matched = pathname.match(route)
-
-            if ( ! matched )
-                return false
+    // vars = { $placeholder: 'value', ... }
+    if (matched) {
+        for (let i = 0; i < len; ) {
+            vars[ placeholders[i++] ] = matched[i]
         }
-        // Strict match.
-        else if (pathname !== route) {
-            return false
-        }
-
-        if (matched) {
-            // Calculate the length once.
-            const len = keys.length
-            // Link each key from parsed route with found route variable.
-            for (let i = 0; i < len;)
-                vars[ keys[i++] ] = matched[i]
-        }
-
-        return vars
     }
+
+    return vars
 }
