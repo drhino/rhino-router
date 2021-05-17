1e1c17e11161ae62a980167b605fec9f64fa0c7e
Gerd <gerdachternaam@gmail.com>
2021-05-17 22:32:24 +0200

    simpler example

--- examples/frontend/controllers/BlogIndex.js
+++ examples/frontend/controllers/BlogIndex.js
@@ -1,5 +1,3 @@
-import { template } from '../module/template.js'
-
 /**
  * BlogIndexController
  */
@@ -10,9 +8,18 @@ export default class {
      */
     static async handle() {
 
-        let html = await template('blog-index.html', {
-            title: 'My Blog !'
-        })
+        const html = `<h1>BlogIndexController</h1>
+        <ul>
+            <li>
+                <a data-handle href="/blog/my-article-1">Article 1</a>
+            </li>
+            <li>
+                <a data-handle href="/blog/my-article-2">Article 2</a>
+            </li>
+            <li>
+                <a data-handle href="/blog/my-article-3">Article 3</a>
+            </li>
+        </ul>`
 
         return html
     }
